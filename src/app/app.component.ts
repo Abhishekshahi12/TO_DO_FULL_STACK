b75cb088bf1f37315085de1a2ebab6fb71be5614
Abhishekshahi12 <abhishekshahi213@gmail.com>
2024-10-25 23:21:21 +0530

    login component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import {LoginComponent} from './login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,WelcomeComponent,LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
+
 export class AppComponent {
   title = 'to-do-project';
+  welcome='It is under app.component.ts';
 }
